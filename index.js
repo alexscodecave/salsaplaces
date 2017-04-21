1a8ec30adee73f56625c6981c230e78e491f9a82
eldiablotrabajo <alexbiddle72@gmail.com>
2017-04-22 00:41:54 +0100

    Removed passport added mongo database connection

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 const queryAPI = require("./public/routes/searchsalsa")
 const path = require("path");
-
+const User = require("./public/models/userSchema")
+const databaseURL = require("./public/routes/secretfile")
+const mongoose = require("mongoose")
+const bParser = require("body-parser")
+app.use(bParser.json());
+app.use(bParser.urlencoded({
+    extended: true
+}));
 app.set('view engine', 'ejs')
 app.set('views',path.join(__dirname,'/public' ))
 
@@ -10,8 +17,23 @@ app.get("/", function(req,res){
     res.render("html/home")
 })
 
-app.get("/register", function(req,res){
+app.get("/registerpage", function(req,res){ 
     res.render("html/register")
+})
+
+app.post("/registeruser", function(req,res){
+    mongoose.connect(`mongodb://${databaseURL.dbName}:${databaseURL.dbPassword}@ds161018.mlab.com:61018/salsaapp`)
+    let newUser = new User({
+        email:req.body.email,
+        username:req.body.username,
+        password:req.body.password
+    })
+    newUser.save(function(err){
+        if(err) console.log(err)
+        else{
+            console.log("User created")
+        }
+    })
 })
 
 app.get("/searchsalsa", queryAPI.searchSalsa)
--- public/models/userSchema.js
+++ public/models/userSchema.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-
+mongoose.Promise = global.Promise;
 const users= mongoose.Schema({
-    username:{type:String,required:true,unique:true},
     email:{type:String,required:true},
+    username:{type:String,required:true,unique:true},
     password:String
 })
 
-module.exports = mongoose.model('User',users)
+const userModel = mongoose.model('User',users)
+
+module.exports = userModel;
